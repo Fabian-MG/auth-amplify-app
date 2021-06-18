b831e06a64cdffc3f9367b022751d6c6701ad91b
Fabian-MG <mirandafabian1510@gmail.com>
2021-06-17 20:29:15 -0500

    Using aws API for triggering the lambda function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { API } from 'aws-amplify'
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+  const [coins, setCoins] = useState([])
+  const { limit, start } = input
+
+  async function fetchCoins() {
+      const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
+      setCoins(data.coins)
+  }
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value })
+  }
+
+  return (<div className="App">
+      <h1>Hello from AWS Amplify 🚀</h1>
+      <div>
+        <input
+          onChange={e => updateInputValues('limit', e.target.value)}
+          placeholder="limit"
+        />
+        <input
+          placeholder="start"
+          onChange={e => updateInputValues('start', e.target.value)}
+        />
+        <button onClick={fetchCoins}>Fetch Coins</button>
+      </div>
+      {
+         coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div> ))
+      }
     </div>
   );
 }
