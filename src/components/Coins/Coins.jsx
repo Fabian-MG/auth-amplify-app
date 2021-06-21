f6441df81cd93e94b2a225b2c0fed40828a559be
Fabian-MG <mirandafabian1510@gmail.com>
2021-06-20 23:36:22 -0500

    Moving coins list to own component

---
+++ src/components/Coins/Coins.jsx
@@ -0,0 +1,44 @@
+import { API } from 'aws-amplify'
+import { useEffect, useState } from 'react';
+
+export const Coins = () => {
+    const [input, updateInput] = useState({ limit: 5, start: 0 })
+    const [coins, setCoins] = useState([])
+    const { limit, start } = input
+  
+    async function fetchCoins() {
+        const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
+        setCoins(data.coins)
+    }
+  
+    useEffect(() => {
+      fetchCoins()
+    }, [])
+  
+    function updateInputValues(type, value) {
+      updateInput({ ...input, [type]: value })
+    }
+
+    return (
+        <>
+            <div>
+            <input
+            onChange={e => updateInputValues('limit', e.target.value)}
+            placeholder="limit"
+            />
+            <input
+            placeholder="start"
+            onChange={e => updateInputValues('start', e.target.value)}
+            />
+            <button onClick={fetchCoins}>Fetch Coins</button>
+            </div>
+            {
+                coins.map((coin, index) => (
+                <div key={index}>
+                <h2>{coin.name} - {coin.symbol}</h2>
+                <h5>${coin.price_usd}</h5>
+                </div> ))
+            }
+        </>
+    )
+}
