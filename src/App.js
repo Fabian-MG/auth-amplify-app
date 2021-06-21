a2acf55873484293d60997645bb8d94ecc6ccc7a
Fabian-MG <mirandafabian1510@gmail.com>
2021-06-20 23:37:59 -0500

    Using NoteList component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import './App.css';
-import { API } from 'aws-amplify'
-import { useEffect, useState } from 'react';
+import { NoteList, Coins } from './components';
 
 function App() {
-  const [input, updateInput] = useState({ limit: 5, start: 0 })
-  const [coins, setCoins] = useState([])
-  const { limit, start } = input
-
-  async function fetchCoins() {
-      const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
-      setCoins(data.coins)
-  }
-
-  useEffect(() => {
-    fetchCoins()
-  }, [])
-
-  function updateInputValues(type, value) {
-    updateInput({ ...input, [type]: value })
-  }
-
-  return (<div className="App">
+ 
+  return (
+    <div className="App">
       <h1>Hello from AWS Amplify 🚀</h1>
-      <div>
-        <input
-          onChange={e => updateInputValues('limit', e.target.value)}
-          placeholder="limit"
-        />
-        <input
-          placeholder="start"
-          onChange={e => updateInputValues('start', e.target.value)}
-        />
-        <button onClick={fetchCoins}>Fetch Coins</button>
-      </div>
-      {
-         coins.map((coin, index) => (
-          <div key={index}>
-            <h2>{coin.name} - {coin.symbol}</h2>
-            <h5>${coin.price_usd}</h5>
-          </div> ))
-      }
+      <NoteList />
     </div>
   );
 }
