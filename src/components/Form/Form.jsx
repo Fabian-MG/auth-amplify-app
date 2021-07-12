61aee1e45c7ac07bd73f9c0c40838476299ae648
Fabian-MG <mirandafabian1510@gmail.com>
2021-07-11 22:46:52 -0500

    Using useAuth hook for user and authentication methods

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react"
+import { useAuth } from "../../hooks/user"
+import { SignUp } from "../SignUp/SignUp"
+import { SignIn } from "../SignIn/SignIn"
+
 
 const initialState = {
     username: '', 
@@ -10,10 +14,27 @@ const initialState = {
 export const Form = () => {
     const [state, setState] = useState(initialState)
     const [formType, setFormType] = useState('signIn')
+    const { signUp, signIn} = useAuth()
+
+    const updateForm = (event) => {
+        const {name, value} = event.target
+        setState({...state, [name]: value})
+    }
+
+    const renderForm = () => {
+        switch(formType) {
+            case 'signUp': 
+                return <SignUp updateFormState={updateForm} signUp={() => signUp(state)}/>
+            case 'signIn':
+                return <SignIn updateFormState={updateForm} signIn={() => signIn(state)}/>
+            default:
+                return null
+        }
+    }
 
     return (
         <div>
-            
+            {renderForm()}
         </div>
     )
 }
