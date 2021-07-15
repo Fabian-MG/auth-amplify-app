699f492cbc93513caa985aa8553c9b166721f968
Fabian-MG <mirandafabian1510@gmail.com>
2021-07-14 19:02:52 -0500

    Changing button style and configuration

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,10 @@
-export const Button = ({onClick, title}) => {
+import { Spinner } from "../Spinner/Spinner"
+
+export const Button = ({onClick, title, loading = false}) => {
     return (
-        <button className="" onClick={onClick}>
-            {title}
+        <button type="submit" className=" flex justify-center items-center opacity-70 w-full mt-5 p-2 rounded-md bg-indigo-600 text-white hover:opacity-100">
+            { loading && <Spinner/> }
+            <span className={`${loading ? 'ml-4' : ''} font-bold`}>{title}</span>
         </button>
     )
 }
